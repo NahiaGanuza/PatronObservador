de9470e8cde868783ff1d793f84b66dda54c8a53
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2024-12-07 11:17:38 +0000

    Add files via upload

---
+++ Patron observador/src/YouTubeChannel.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class YouTubeChannel {
+    private List<Observer> observers = new ArrayList<>();
+    private String searchQuery;
+
+    public YouTubeChannel(String searchQuery) {
+        this.searchQuery = searchQuery;
+    }
+
+    public void subscribe(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void unsubscribe(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(String videoTitle, String channelTitle) {
+        for (Observer observer : observers) {
+            observer.update(videoTitle, channelTitle);
+        }
+    }
+
+    public String getSearchQuery() {
+        return searchQuery;
+    }
+}
